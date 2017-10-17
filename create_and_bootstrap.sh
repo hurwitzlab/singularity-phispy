833d586047e0c1c32282cfdb8ed26bdd130e1d20
Scott G. Daniel <scottdaniel25@gmail.com>
2017-10-17 23:37:22 +0000

    more space

--- create_and_bootstrap.sh
+++ create_and_bootstrap.sh
@@ -5,7 +5,7 @@ set -x
 echo "Hello! starting $(date)"
 
 sudo rm -rf phispy.img
-singularity create -s 1234 phispy.img
+singularity create -s 2048 phispy.img
 sudo singularity bootstrap phispy.img ubuntu.sh
 
 echo "Goodbye! ending $(date)"
