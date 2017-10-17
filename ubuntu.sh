4aec36bed9ad16bc4799a19476ac7d23ea944aa1
Scott G. Daniel <scottdaniel25@gmail.com>
2017-10-17 23:29:06 +0000

    moving along

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,10 +3,10 @@ OSVersion: trusty
 MirrorURL: http://us.archive.ubuntu.com/ubuntu/
 
 %environment
-    PATH="/apps:/apps/miniconda/bin:$PATH"
+    PATH="/apps/PhiSpy:/apps/miniconda/bin:$PATH"
 
 %runscript
-    exec echo "ehllo"
+    exec python /apps/PhiSpy.py
 
 %post
     echo "Hello from inside the container"
@@ -21,18 +21,19 @@ MirrorURL: http://us.archive.ubuntu.com/ubuntu/
     cd /apps
     #Miniconda for cutadapt / trimgalore
     #and if trimgalore works better we might just get rid of solexaqa
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-    bash miniconda.sh -b -p /apps/miniconda
-    PATH="/apps:/apps/miniconda/bin:$PATH"
-    conda install -y -f -q -c bioconda biopython
-    conda install -y -f -q -c bioconda R
-    conda install -y -f -q -c bioconda r-randomforest
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
+    bash Miniconda2-latest-Linux-x86_64.sh -b -p /apps/miniconda
+    rm Miniconda2-latest-Linux-x86_64.sh
+    sudo ln -s /apps/miniconda/bin/python2.7 /usr/bin/python
+    PATH="/apps/PhiSpy:/apps/miniconda/bin:$PATH"
+    conda install -y -c bioconda biopython
+    conda install -y -c bioconda R
+    conda install -y -c bioconda r-randomforest
     
     git clone https://github.com/linsalrob/PhiSpy.git
     cd PhiSpy
     make
-    wget https://github.com/linsalrob/PhiSpy/blob/master/Test_Organism.zip
-    gunzip ./Test_Organism.zip
+    unzip -q ./Test_Organism.zip
 
     #cleanup    
     conda clean -y --all
@@ -48,5 +49,6 @@ MirrorURL: http://us.archive.ubuntu.com/ubuntu/
     mkdir /xdisk
 
 %test
-    cd PhiSpy
+    PATH="/apps/PhiSpy:/apps/miniconda/bin:$PATH"
+    cd /apps/PhiSpy
     ./PhiSpy.py -i Test_Organism/160490.1/ -o output_directory -t 25
